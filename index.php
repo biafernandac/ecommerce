cf89a4f788c49d1902a125a93b95d5eb72279acf
Bia Fernanda <biachava@gmail.com>
2020-10-13 23:46:13 -0300

    add produtos carrinho de compras

--- index.php
+++ index.php
@@ -29,6 +29,8 @@ $app->get('/', function() {
 
 });
 
+// ROTAS DO CADASTRO DE USUÁRIOS PELO ADMIN
+
 $app->get('/admin', function() {
 
 	User::verifyLogin();
@@ -487,9 +489,64 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
 
 });
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i=0; $i < $qtd; $i++) { 
+		
+		$cart->addProduct($product);
+
+	}
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProducts($product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProducts($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
+
 
 $app->run();
 
